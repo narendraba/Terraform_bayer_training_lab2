ba1c78153dbe991130b72e55a89f48116796c172
narendraba <ballipallinarendra@gmail.com>
2024-12-18 15:29:08 +0530

    Update main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-bucket-78910"
+    bucket         = "spliting-bucket"
     key            = "networking/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
