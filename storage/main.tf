c45ba38fd2e868ebc838c4770beeb43f5ab17a65
narendraba <ballipallinarendra@gmail.com>
2024-12-18 12:01:25 +0530

    Create main.tf

---
+++ storage/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-bucket-789"
+    key            = "storage/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "networking"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "storage" {
+  bucket = "my-storage-bucket124"
+  tags = {
+    Name = "storage-bucket"
+  }
+}
