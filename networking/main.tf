87cc7da10c1d3b7ffa155ea900023f5f9df91246
narendraba <ballipallinarendra@gmail.com>
2024-12-18 11:25:23 +0530

    Create main.tf

---
+++ networking/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-bucket-789"
+    key            = "networking/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "networking"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "networking" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "networking-vpc"
+  }
+}
