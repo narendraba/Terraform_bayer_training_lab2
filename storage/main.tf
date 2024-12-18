68a7e097021cace538cefe2aad8beb4f3f7d6dc3
narendraba <ballipallinarendra@gmail.com>
2024-12-18 12:25:21 +0530

    Update main.tf

--- storage/main.tf
+++ storage/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-bucket-789"
+    bucket         = "terraform-bucket-78910"
     key            = "storage/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
