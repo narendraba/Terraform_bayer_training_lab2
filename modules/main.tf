2b9ee11b4754b36b8407fc144e22dff58fbbbde5
narendraba <ballipallinarendra@gmail.com>
2024-12-19 21:12:39 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
   source = "git::https://github.com/narendraba/private_module_bayer.git"
 
-  cidr_block         = "10.0.0.0/16"
-  public_subnets     = ["10.0.1.0/24", "10.0.3.0/24"]
-  private_subnets    = ["10.0.2.0/24", "10.0.4.0/24"]
-  availability_zones = ["ap-south-1a", "ap-south-1b"]
+  #cidr_block         = "10.0.0.0/16"
+  #public_subnets     = ["10.0.1.0/24", "10.0.3.0/24"]
+  #private_subnets    = ["10.0.2.0/24", "10.0.4.0/24"]
+  #availability_zones = ["ap-south-1a", "ap-south-1b"]
 }
