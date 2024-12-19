f89fc90af7f158f183dba366b8056319054e3473
narendraba <ballipallinarendra@gmail.com>
2024-12-19 21:06:27 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::github.com/narendraba/private_module_bayer//?ref=v1.0.0"
+  source = "git::https://github.com/narendraba/private_module_bayer.git"
 
   cidr_block         = "10.0.0.0/16"
   public_subnets     = ["10.0.1.0/24", "10.0.3.0/24"]
