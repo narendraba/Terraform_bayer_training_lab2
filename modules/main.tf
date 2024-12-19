79ce6402b0fa2f8061f807e2b3630188d701fe86
narendraba <ballipallinarendra@gmail.com>
2024-12-19 19:39:17 +0530

    Create main.tf

---
+++ modules/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source = "git::ssh://git@github.com/narendraba/private_module_bayer.git//vpc-module"
+
+  cidr_block         = "10.0.0.0/16"
+  public_subnets     = ["10.0.1.0/24", "10.0.3.0/24"]
+  private_subnets    = ["10.0.2.0/24", "10.0.4.0/24"]
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
+}
